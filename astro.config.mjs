5d13c47b501841810f7b066c3382663a1c8ce49a
digran.dev <90892831+KevinDigran97@users.noreply.github.com>
2024-11-29 18:21:53 -0500

    update data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  alias: {
+    '@cv': './src/components/cv', // Cambia la ruta al directorio correcto
+  },
+});
