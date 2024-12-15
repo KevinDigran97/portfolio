16d5b881aaa540d58cc35d04f6669155fbd739e2
digran.dev <90892831+KevinDigran97@users.noreply.github.com>
2024-12-15 00:26:35 -0500

    Vercer-Server-add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
-
   output: 'server',
   adapter: vercel(),
   alias: {
-    '@cv': './src/components/cv', // Cambia la ruta al directorio correcto
-  },
+    '@cv': './src/components/cv' // Cambia la ruta al directorio correcto
+  }
 });
